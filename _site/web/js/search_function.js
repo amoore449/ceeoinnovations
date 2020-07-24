d888d4f9ec3ec6df2f7159380e18a26688957eef
amoore449 <moore.m.allison@gmail.com>
2020-07-24 16:18:41 -0700

    search

--- _site/web/js/search_function.js
+++ _site/web/js/search_function.js
@@ -4,7 +4,7 @@ function mySearchFunction() {
   searchtext = searchtext.toLowerCase();
   //console.log(document.getElementById("searchinput").value);
   //gatherallprojects
-  var allprojects= document.getElementsByClassName("projectsegment");
+ let allprojects= document.getElementsByClassName("projectsegment");
 
   //for each projects
   for(let i = 0; i < allprojects.length; i++){
@@ -21,41 +21,38 @@ function mySearchFunction() {
 }
 
 function DisplayAllProjects() {
-     var allprojects= document.getElementsByClassName("projectsegment");
-
-     //for each projects
-     for(let i = 0; i < allprojects.length; i++){
-          $(allprojects[i]).css({"display": "inline"});
+     let allmyprojects= document.getElementsByClassName("projectsegment");
+     for(let i = 0; i < allmyprojects.length; i++){
+          $(allmyprojects[i]).css({"display": "inline"});
      }
 
 }
 
-function ButtonStart(){
+function ButtonStart(buttonelement){
      console.log("ButtonStart");
-var my_button_array = document.getElementsByClassName("myButton");
-for (let i = 0; i < my_button_array.length; i++){
-    var mybuttontext = my_button_array[i].innerHTML;
-    if(my_button_array[i].id != "All"){
-         console.log("button_class");
-         my_button_array[i].onclick = function() {ButtonAssigner(my_button_array[i])};
-    } else{
+     console.log(buttonelement.innerHTML);
+
+    if(buttonelement.getElementsByTagName("h6")[0].innerHTML!= "All"){
+        ButtonAssigner(buttonelement);
+    } else {
          DisplayAllProjects();
     }
 }
-}
 function ButtonAssigner(button) {
+     console.log(button.getElementsByTagName("h6")[0].innerHTML);
 
-     document.getElementById('search-input').value="";
+     let buttontext = button.getElementsByTagName("h6")[0].innerHTML.toLowerCase();
+     let allprojects= document.getElementsByClassName("projectsegment");
 
-     for (let i = 0; i < my_button_array.length; i++){
-          var mybutton_id = my_button_array[i].id;
-          var button_class = document.getElementsByClassName(mybutton_id);
-          console.log(button_class);
-            $(button_class).css({"display": "none"});
-       }
-      var button_id = button.id;
+     //for each projects
+     for(let i = 0; i < allprojects.length; i++){
+          let projecttags = allprojects[i].getElementsByClassName("hidden-tags")[0];
+          projecttags = projecttags.innerHTML.toLowerCase();
+          if(projecttags.includes(buttontext)){
+               $(allprojects[i]).css({"display": "inline"});
+          } else {
+               $(allprojects[i]).css({"display": "none"});
+          }
 
-
-      var class_arr = document.getElementsByClassName(button_id);
-      $(class_arr).css({"display": "inline"});
+      }
 }
