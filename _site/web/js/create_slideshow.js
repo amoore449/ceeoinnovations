40d5898c2e1b8529be8822214977568fc56a24d8
amoore449 <moore.m.allison@gmail.com>
2020-07-28 15:06:48 -0700

    redesign

--- _site/web/js/create_slideshow.js
+++ _site/web/js/create_slideshow.js
@@ -2,45 +2,44 @@
 * Allison Moore
 * 7/23/20
 */
+
+
 function CreateCaroMain(){
-     console.log("platup");
      var myslidecontainer = document.getElementsByClassName("sample_caro")[0];
      var myslides = myslidecontainer.getElementsByClassName('mysamplecaro');
      var carousel = document.getElementsByClassName('caro');
 
-     var newslidecontain = document.createElement("div");
-     newslidecontain.className = "carousel js-flickity";
-
+     var newslidecontain = document.getElementsByClassName('carousel')[0];
      for(let i = 0; i < myslides.length; i++){
           var newslide = document.createElement("div");
           newslide.className = "carousel-cell";
           newslide.innerHTML = myslides[i].innerHTML;
 
 
-          i++;
-          if(i<myslides.length){
-               var newslide2 = document.createElement("div");
-               ;
-               newslide2.innerHTML = myslides[i].innerHTML;
-               newslide.append(newslide2);
-          }
-          i++;
-          if(i<myslides.length){
-               var newslide3 = document.createElement("div");
-
-               newslide3.innerHTML = myslides[i].innerHTML;
-               newslide.append(newslide3);
-          }
-          i++;
-          if(i<myslides.length){
-               var newslide3 = document.createElement("div");
-
-               newslide3.innerHTML = myslides[i].innerHTML;
-               newslide.append(newslide3);
-          }
 
           newslidecontain.append(newslide);
 
+     }
+
+     //carousel[0].append(newslidecontain);
+
+     //CreateSmallCaroMain();
+}
+
+function CreateSmallCaroMain(){
+     let myslidecontainer = document.getElementsByClassName("sample_caro")[0];
+     let myslides = myslidecontainer.getElementsByClassName('mysamplecaro');
+     let carousel = document.getElementsByClassName('caroSMALL');
+
+     let newslidecontain = document.createElement("div");
+     newslidecontain.className = "carousel js-flickity";
+
+     for(let i = 0; i < myslides.length; i++){
+          let newslide = document.createElement("div");
+          newslide.className = "carousel-cell";
+          newslide.innerHTML = myslides[i].innerHTML;
+
+          newslidecontain.append(newslide);
      }
 
      carousel[0].append(newslidecontain);
